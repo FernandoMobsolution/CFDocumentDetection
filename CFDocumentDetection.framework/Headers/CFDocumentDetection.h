e46b28b5b559935c6e642ad8d7f6b0abd766ca91
Fernando Fiorentin <fernando@mobsolution.com.br>
2020-04-27 15:24:46 -0300

    Commit inicial

---
+++ CFDocumentDetection.framework/Headers/CFDocumentDetection.h
@@ -0,0 +1,19 @@
+//
+//  CFDocumentDetection.h
+//  CFDocumentDetection
+//
+//  Created by Tiago Portaluppi on 26/09/19.
+//  Copyright © 2019 Combate a Fraude. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CFDocumentDetection.
+FOUNDATION_EXPORT double CFDocumentDetectionVersionNumber;
+
+//! Project version string for CFDocumentDetection.
+FOUNDATION_EXPORT const unsigned char CFDocumentDetectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CFDocumentDetection/PublicHeader.h>
+
+
